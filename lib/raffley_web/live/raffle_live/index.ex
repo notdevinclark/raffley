ca38431f80f8163b1ea6c579b3c94f2b194c2ed1
Devin Clark <notdevinclark@gmail.com>
2025-01-11 20:41:56 -0800

    chp14: move card and badge into function components

--- lib/raffley_web/live/raffle_live/index.ex
+++ lib/raffley_web/live/raffle_live/index.ex
@@ -12,19 +12,35 @@ defmodule RaffleyWeb.RaffleLive.Index do
     ~H"""
     <div class="raffle-index">
       <div class="raffles">
-        <div :for={raffle <- @raffles} class="card">
-          <img src={raffle.image_path} />
-          <h2>{raffle.prize}</h2>
-          <div class="details">
-            <div class="price">
-              ${raffle.ticket_price} / ticket
-            </div>
-            <div class="badge">
-              {raffle.status}
-            </div>
-          </div>
-        </div>
+        <.raffle_card :for={raffle <- @raffles} raffle={raffle} />
       </div>
+    </div>
+    """
+  end
+
+  attr :raffle, Raffley.Raffle, required: true
+
+  def raffle_card(assigns) do
+    ~H"""
+    <div class="card">
+      <img src={@raffle.image_path} />
+      <h2>{@raffle.prize}</h2>
+      <div class="details">
+        <div class="price">
+          ${@raffle.ticket_price} / ticket
+        </div>
+        <.badge status={@raffle.status} />
+      </div>
+    </div>
+    """
+  end
+
+  attr :status, :atom, required: true, values: [:upcoming, :open, :closed]
+
+  def badge(assigns) do
+    ~H"""
+    <div class="badge">
+      {@status}
     </div>
     """
   end
